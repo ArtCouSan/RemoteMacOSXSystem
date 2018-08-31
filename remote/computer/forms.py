2a8c76e55ff7f29ac3b6ee868f6dc48ea664a414
artcousan <artcousan@gmail.com>
2018-08-31 17:33:19 -0300

    CRUD pronto

--- remote/computer/forms.py
+++ remote/computer/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from computer.models import Computer
 
 class ComputerForm(forms.Form):
 
@@ -12,6 +13,14 @@ class ComputerForm(forms.Form):
         valid = True
         if not super(ComputerForm, self).is_valid():
             self.add_error('Verifique os campos')
+            valid = False
+
+        return valid
+        
+        computer = Computer.objects.get(ip = self.data['ip'])
+
+        if computer.ip:
+            self.add_error('IP existente')
             valid = False
 
         return valid
