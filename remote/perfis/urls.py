15489cdf491f7f61bf98025c7dfa40a61cb01121
artcousan <artcousan@gmail.com>
2018-08-31 16:46:12 -0300

    CRUD pronto

--- remote/perfis/urls.py
+++ remote/perfis/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'django.contrib.auth.views.login', { "template_name" : "login.html"  },  name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', { "login_url" : "login.html"  }, name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^home/$', 'perfis.views.home', name='home')
 )
 
